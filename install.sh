cb4f1a3b10ced9d11744c28f3e711e63adec64a3
alex <alex@alexs-MacBook-Pro.local>
2024-09-23 19:29:25 +0100

    Update to use any desktop as long as it is like debian. version check may be wrong

--- install.sh
+++ install.sh
@@ -1,13 +1,45 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+
+# Function to detect the desktop environment
+detect_desktop_environment() {
+    if [ -n "$XDG_CURRENT_DESKTOP" ]; then
+        echo "Desktop Environment: $XDG_CURRENT_DESKTOP"
+        return 0
+    elif [ -n "$GNOME_DESKTOP_SESSION_ID" ]; then
+        echo "Desktop Environment: GNOME"
+        return 0
+    elif [ -n "$KDE_FULL_SESSION" ]; then
+        echo "Desktop Environment: KDE"
+        return 0
+    elif [ -n "$MATE_DESKTOP_SESSION_ID" ]; then
+        echo "Desktop Environment: MATE"
+        return 0
+    elif [ -n "$XDG_SESSION_DESKTOP" ]; then
+        echo "Desktop Environment: $XDG_SESSION_DESKTOP"
+        return 0
+    else
+        echo "Desktop Environment: Unknown"
+        return 1
+    fi
+}
+
+# Detect if any desktop environment is running
+if detect_desktop_environment; then
+    DESKTOP_RUNNING=true
+else
+    DESKTOP_RUNNING=false
+fi
+
 # Desktop software and tweaks will only be installed if we're running Gnome
-RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
+# RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
 
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/omakub/install/check-version.sh
 
-if $RUNNING_GNOME; then
+# if $RUNNING_GNOME; then
+if $DESKTOP_RUNNING; then
   # Ensure computer doesn't go to sleep or lock while installing
   gsettings set org.gnome.desktop.screensaver lock-enabled false
   gsettings set org.gnome.desktop.session idle-delay 0
@@ -24,7 +56,8 @@ fi
 # Install terminal tools
 source ~/.local/share/omakub/install/terminal.sh
 
-if $RUNNING_GNOME; then
+# if $RUNNING_GNOME; then
+if $DESKTOP_RUNNING; then
   # Install desktop tools and tweaks
   source ~/.local/share/omakub/install/desktop.sh
 

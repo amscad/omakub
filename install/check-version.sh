693882177d6807e16abad3daf81596b76b660037
alex <ajmansfield@gmail.com>
2024-09-23 19:46:44 +0100

    Fix check for running debian like system

--- install/check-version.sh
+++ install/check-version.sh
@@ -7,10 +7,11 @@ if [ ! -f /etc/os-release ]; then
 fi
 
 . /etc/os-release
+echo $ID_LIKE 
 
 # Check if running on Ubuntu 24.04 or higher
 # if [ "$ID" != "ubuntu" ] || [ $(echo "$VERSION_ID >= 24.04" | bc) != 1 ]; then
-if [[ "ID_LIKE" != *"debian"* ]]; then
+if [[ "$ID_LIKE" != *"debian"* ]]; then
     echo "$(tput setaf 1)Error: OS requirement not met"
     echo "You are currently running: $ID $VERSION_ID"
     echo "OS required: ubuntu 24.04 or higher"

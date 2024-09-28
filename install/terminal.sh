5a3e2bf1c042e07e67239b43ac418cf0fec999be
Alex <amscad@users.noreply.github.com>
2024-09-28 11:43:03 +0100

    Install oh-my-zsh
    Implementation sucks, but Im too lazy to change it

--- install/terminal.sh
+++ install/terminal.sh
@@ -5,3 +5,4 @@ sudo apt install -y curl git unzip
 
 # Run terminal installers
 for installer in ~/.local/share/omakub/install/terminal/*.sh; do source $installer; done
+for installer in ~/.local/share/omakub/install/terminal/optional/app-zsh.sh; do source $installer; done
